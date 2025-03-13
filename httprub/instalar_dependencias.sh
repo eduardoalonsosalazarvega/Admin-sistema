4bd1752c59a4a99cfe92462ce9489413cde3be5b
Eduardo Alonso Salazar Vega <eduardoalonsosalazarvega3@gmail.com>
2025-03-13 00:41:10 -0700

    instlar

--- httprub/instalar_dependencias.sh
+++ httprub/instalar_dependencias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 instalar_dependenciashttp(){ 
     # Actualizar lista de paquetes
-    sudo apt update -y > /dev/null 2>&1
+    sudo apt update -y #> /dev/null 2>&1
     
     # Lista de dependencias necesarias
     dependencias=("libapr1" "libapr1-dev" "libaprutil1" "libaprutil1-dev" "build-essential" "wget" "libpcre3" "libpcre3-dev" "libssl-dev" "zlib1g-dev")
@@ -9,7 +9,7 @@ instalar_dependenciashttp(){
     # Verificar e instalar cada dependencia si no está instalada
     for paquete in "${dependencias[@]}"; do
         if ! dpkg -l | grep -qw "$paquete"; then
-            sudo apt-get install -y "$paquete" > /dev/null 2>&1
+            sudo apt-get install -y "$paquete" #> /dev/null 2>&1
         fi
     done
 }
