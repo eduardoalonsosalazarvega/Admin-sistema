2a2f30807259e8a9c97fd2c5e17ee168dffa52fc
Eduardo Alonso Salazar Vega <eduardoalonsosalazarvega3@gmail.com>
2025-03-13 17:34:16 -0700

    aa

--- httprub/menu_principal.sh
+++ httprub/menu_principal.sh
@@ -15,13 +15,13 @@ sudo apt install net-tools -y > /dev/null 2>&1
 
 while true; do
     menu_http
-    read -p "Elija el servicio HTTP que desea configurar (1-3): " op
+    read -p "Selecciona el servicio HTTP que desea instalar (1-3): " op
             
     if [ "$op" -eq 1 ]; then
         versions=$(obtener_version "Apache")
         stable=$(echo "$versions" | head -1)
         menu_http2 "Apache" "$stable" " "
-        echo "Elija la versión que desea instalar: "
+        echo "Selecciona la version que desea instalar:: "
         op2=$(solicitar_ver "Apache") 
         if [ "$op2" -eq 1 ]; then
             port=$(solicitar_puerto)
@@ -37,7 +37,7 @@ while true; do
         stable=$(echo "$versions" | tail -n 2 | head -1)
         mainline=$(echo "$versions" | tail -1)
         menu_http2 "Nginx" "$stable" "$mainline"
-        echo "Elija la versión que desea instalar: "
+        echo "Selecciona la version que desea instalar:: "
         op2=$(solicitar_ver "Nginx")
         if [ "$op2" -eq 1 ]; then  
             port=$(solicitar_puerto)
@@ -59,7 +59,7 @@ while true; do
         stable=$(echo "$versions" | tail -n 2 | head -1)
         mainline=$(echo "$versions" | tail -1)
         menu_http2 "OpenLiteSpeed" "$stable" "$mainline"
-        echo "Elija la versión que desea instalar: "
+        echo "Selecciona la version que desea instalar: "
         op2=$(solicitar_ver "OpenLiteSpeed")
         if [ "$op2" -eq 1 ]; then
             port=$(solicitar_puerto)
@@ -80,6 +80,6 @@ while true; do
         echo "Saliendo..."
         exit 0
     else
-        echo "Opción no válida. Intente de nuevo."
+        echo "Opción no valida.Vuelve a intentar"
     fi
 done
--- httprub/solicitarpuerto.sh
+++ httprub/solicitarpuerto.sh
@@ -4,17 +4,17 @@ solicitar_puerto() {
     local puertos_reservados=(1433 1434 1521 2049 3128 3306 3389 5432 6000 6379 6660 6661 6662 6663 6664 6665 6666 6667 6668 6669 27017 8000 8080 8888)
     
     while true; do
-        read -p "Introduce un puerto válido (1-65535): " port
+        read -p "Porfavorcitooo ingresa un puerto que este entre  (1-65535): " port
 
         [[ -z "$port" ]] && return
 
         # Verificar si el input es un número y está en el rango permitido
         if ! [[ "$port" =~ ^[0-9]+$ ]] || [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; then
-            echo "Puerto no válido. Debe estar entre 1 y 65535." >&2  
+            echo "lo hiciste mal, intentalo nuevame . El rango debe estar entre 1 y 65535." >&2  
 
         # Verificar si el puerto está en la lista de reservados
         elif [[ " ${puertos_reservados[*]} " =~ " $port " ]]; then
-            echo "Puerto $port está reservado para otro servicio. Intenta con otro." >&2
+            echo "Puerto $port está reservado para algun otro servicio. Intenta con otro plis." >&2
 
         # Verificar si el puerto ya está en uso con ss
         elif ss -tuln | grep -q ":$port "; then
